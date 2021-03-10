dcc5e92b73227aa68a1cc5e5e4989b4bb3ce844e
L0phTg <L0phTg@163.com>
2021-03-10 15:57:54 +0800

    rebuilding site 2021年03月10日 15:57:53

--- service-worker.js
+++ service-worker.js
@@ -39,6 +39,7 @@ var filesToCache = [
   // MathJax
   'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-AMS-MML_HTMLorMML',
   'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/config/TeX-AMS-MML_HTMLorMML.js?V=2.7.5',
+
 ];
 
 // Cache the application assets
